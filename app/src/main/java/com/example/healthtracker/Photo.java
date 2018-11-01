621510413abb347d8c3fc00e5e3a1b5102918f1a
foreverznb <32942531+foreverznb@users.noreply.github.com>
2018-11-01 01:43:01 -0600

    class photo

--- app/src/main/java/com/example/healthtracker/Photo.java
+++ app/src/main/java/com/example/healthtracker/Photo.java
@@ -5,4 +5,28 @@ package com.example.healthtracker;
  */
 
 public class Photo {
+    private String fileLocation;
+    private Record parentRecord;
+
+    public void Photo(String fileLocation,Record parentRecord){
+        this.fileLocation = fileLocation;
+        this.parentRecord = parentRecord;
+    }
+
+    public String getFile(){
+        return this.fileLocation;
+    }
+
+    public void setFile(String fileLocation){
+        this.fileLocation = fileLocation;
+    }
+
+    public Record getRecord(){
+        return this.parentRecord;
+    }
+
+
+
+
+
 }
