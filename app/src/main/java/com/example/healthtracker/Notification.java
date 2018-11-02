077b8b4d554b047ffd5f68849a2919a68a9fe640
feiooo <32970921+feiooo@users.noreply.github.com>
2018-11-02 14:32:25 -0600

    Update app/src/main/java/com/example/healthtracker/Notification.java
    Add Notification

---
+++ app/src/main/java/com/example/healthtracker/Notification.java
@@ -0,0 +1,13 @@
+package com.example.healthtracker;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Notification {
+    private Boolean isOn;
+    private Problem parentProblem;
+    private String repeatType;
+    private Time time;
+    private Date startDate;
+
+}
