f8515827344c98082c0ba2440afb6899a982f237
feiooo <32970921+feiooo@users.noreply.github.com>
2018-11-02 14:21:55 -0600

    Update new code

--- app/src/main/java/com/example/healthtracker/PatientRecord.java
+++ app/src/main/java/com/example/healthtracker/PatientRecord.java
@@ -3,42 +3,57 @@ package com.example.healthtracker;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class PatientRecord extends BaseRecord {
+public class PatientRecord extends SimpleRecord {
 
-    private BodyPart bodyPart;
+    //private ArrayList<Photo> photos;
+    private String title;
+    private ArrayList<String> comments;
+    private Problem parentProblem;
 
-    private ArrayList<Photo> photos;
-
-    private ArrayList<Photo> bodyPhotos;
-
-    private Date timestamp;
-
-    private int amountOfPhotos;
+    //private Date timestamp;
+    //private int amountOfPhotos;
 
 
-    public PatientRecord(String title, ArrayList<String> comments, String address, BodyPart bodyPart, ArrayList<Photo> photos, Date timestamp, Problem parentProblem){
-        super(title, comments, parentProblem);
+    public PatientRecord(String title, ArrayList<String> comments, Problem parentProblem){
+        this.comments = super(comments);
 
-        this.bodyPart = bodyPart;
-        this.photos = photos;
-        this.bodyPhotos = bodyPhotos;
-        this.timestamp = timestamp;
+        //this.bodyPart = bodyPart;
+        //this.photos = photos;
+        //this.bodyPhotos = bodyPhotos;
+        //this.timestamp = timestamp;
+        this.parentProblem = parentProblem;
 
     }
 
-    public String getAddress(){
-        String address = "";
-        return address;
+    public void setTitle(String newTitle){
+        this.title = newTitle;
     }
 
-    public void setAddress(String Address){}
-
-    public BodyPart getBodyPart(){
-        return bodyPart;
+    public String getTitle(){
+        return this.title;
     }
 
-    public void setBodyPart(BodyPart bodyPart){}
+    public String getComments(Integer index){ //This is different from Jori's UML
+        return comments.get(index);
+    }
 
+    public void addComment(String comment){
+        comments.add(comment);
+    }
+
+    public void setComment(Integer index,String comment){
+        comments.set(index,comment);
+    }
+
+    public void deleteComment(Integer index){
+        comments.remove(index);
+    }
+
+    public Problem getProblem(){
+        return this.parentProblem;
+    }
+
+    /*
     public ArrayList<Photo> getPhotos(){
 
         return photos;
@@ -50,20 +65,6 @@ public class PatientRecord extends BaseRecord {
 
     public void setPhoto(int index, Photo photo){}
 
-    public Date getTimeStamp(){
-        return timestamp;
-    }
-
-    public void setTimeStamp(){}
-
-    public ArrayList<Photo> getBodyPhotos(){
-        return bodyPhotos;
-    }
-
-    public int getAmountOfPhotos(){
-        return amountOfPhotos;
-    }
-
-
+*/
 
 }
--- app/src/test/java/com/example/healthtracker/PatientRecordTest.java
+++ app/src/test/java/com/example/healthtracker/PatientRecordTest.java
@@ -10,116 +10,65 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 
-public class PatientRecordTest{
-
-    private BodyPart bodyPart;
-    private ArrayList<Photo> photos;
-    private ArrayList<Photo> bodyPhotos;//ArrayList<BodyPhoto>
-    private Date timestamp;
-    private int amountOfPhotos;
+public class PatientRecordTest {
+    private String title;
+    private ArrayList<String> comments;
+    private Problem problem;
     private PatientRecord patientRecord;
-
-
 
     @Before
     public void setUp() throws Exception {
-        String title = "title 1";
-        ArrayList<String>comments = new ArrayList<String>();
+        title = "Record 1";
+        comments = new ArrayList<String>();
         Date dateStarted = new Date();
-        Problem problem = new Problem("Rash",dateStarted,"Red spots are gradually diminishing");
-        String address = "around";
-
-        ArrayList<BodyPhoto> bp = new ArrayList<BodyPhoto>();
-        BaseRecord parentRecord = new BaseRecord(title,comments,problem);
-
-        bodyPart = new BodyPart("Hand",parentRecord);
-        photos = new ArrayList<Photo>();
-        bodyPhotos = new ArrayList<BodyPhoto>();
-
-        timestamp = new Date();
-        amountOfPhotos = 0;
-
-        patientRecord = new PatientRecord(title, comments, address, bodyPart, photos, timestamp, problem);
-    }
-
-
-    @Test
-    public void getAddress() throws Exception{
-        String address = "";
-        assertEquals(address,patientRecord.getAddress());
+        problem = new Problem("Rash",dateStarted,"Red spots are gradually diminishing");
+        patientRecord = new  PatientRecord(title,comments,problem);
     }
 
     @Test
-    public void setAddress()throws Exception{
-        String newAddress = "new address";
-        patientRecord.setAddress(newAddress);
-        assertEquals(newAddress, patientRecord.getAddress());
-    }
-
-
-    @Test
-    public void getBodyPart() throws Exception {
-        assertEquals(bodyPart,patientRecord.getBodyPart());
-    }
-
-
-    @Test
-    public void setBodyPart()throws Exception{
-        BodyPart bodyPart = new BodyPart("Finger", patientRecord);
-        patientRecord.setBodyPart(bodyPart);
-        assertEquals(bodyPart, patientRecord.getBodyPart());
+    public void setTitle() throws Exception {
+        String newTitle = "Record 2";
+        patientRecord.setTitle(newTitle);
+        assertEquals(patientRecord.getTitle(),newTitle);
     }
 
     @Test
-    public void getPhotos()throws Exception{
-        assertEquals(photos,patientRecord.getPhotos());
+    public void getTitle() throws Exception {
+        assertEquals(patientRecord.getTitle(),title);
     }
 
     @Test
-    public void addPhoto()throws Exception{
-        Photo newPhoto = new Photo();
-        patientRecord.addPhoto(newPhoto);
-        //How to test
-    }
-
-    @Test
-    public void removePhoto()throws Exception{
+    public void getComments() throws Exception {
         Integer index = 0;
-        Photo prev_photo = photos.get(index);
-        patientRecord.removePhoto(index);
-        assertFalse(patientRecord.getPhotos().get(index) == prev_photo);
+        assertEquals(patientRecord.getComments(index),comments);
     }
 
     @Test
-    public void setPhoto()throws Exception{
+    public void addComment() throws Exception {
+        String newComment = "new comment";
+        patientRecord.addComment(newComment);
+
+    }
+
+    @Test
+    public void setComment() throws Exception {
+        String newComment = "new comment";
         Integer index = 0;
-        Photo newPhoto = new Photo();
-        Photo prev_photo = photos.get(index);
-        patientRecord.setPhoto(index,newPhoto);
-
-        assertTrue(patientRecord.getPhotos().get(index) == newPhoto && patientRecord.getPhotos().get(index+1) == prev_photo);
+        patientRecord.setComment(index,newComment);
+        assertEquals(patientRecord.getComments(index),"new comment");
     }
 
     @Test
-    public void getTimeStamp()throws Exception{
-        assertEquals(timestamp, patientRecord.getTimeStamp());
+    public void deleteComment() throws Exception {
+        Integer index = 0;
+        String prev_comment = comments.get(index);
+        patientRecord.deleteComment(index);
+        assertFalse(patientRecord.getComments(index) == prev_comment);
     }
 
     @Test
-    public void setTimeStamp()throws Exception{
-        Date timestamp = new Date();
-        //patientRecord.setTimeStamp();
-        assertEquals(timestamp, patientRecord.getTimeStamp());
-    }
-
-    @Test
-    public void getBodyPhotos()throws Exception{
-        assertEquals(bodyPhotos,patientRecord.getBodyPhotos());
-    }
-
-    @Test
-    public void getAmountOfPhotos()throws Exception{
-        assertEquals(amountOfPhotos,patientRecord.getAmountOfPhotos());
+    public void getProblem() throws Exception {
+        assertEquals(problem,patientRecord.getProblem());
     }
 
 
