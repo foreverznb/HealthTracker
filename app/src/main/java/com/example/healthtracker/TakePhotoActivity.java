a28f42be6d1097c09259faf0562de529d1763479
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-11-26 16:09:38 -0700

    adding photo controller

--- app/src/main/java/com/example/healthtracker/TakePhotoActivity.java
+++ app/src/main/java/com/example/healthtracker/TakePhotoActivity.java
@@ -92,8 +92,13 @@ public class TakePhotoActivity extends AppCompatActivity {
                 assert image != null;
                 Bitmap scaledBitmap = Bitmap.createScaledBitmap(image, 300, 300, true);
                 Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
-                pathLoaded=saveToInternalStorage(rotatedBitmap);
-                loadImageFromStorage(pathLoaded);
+                String test = PhotoController.imageToString(rotatedBitmap);
+                imageView.setImageBitmap(PhotoController.stringToImage(test));
+                /*
+                ContextWrapper cw = new ContextWrapper(getApplicationContext());
+                pathLoaded=PhotoController.saveToInternalStorage(rotatedBitmap,cw);
+                Bitmap test1 = PhotoController.loadImageFromStorage(pathLoaded,imageName);
+                textTargetUri.setText(pathLoaded);*/
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
             } else {
@@ -112,9 +117,11 @@ public class TakePhotoActivity extends AppCompatActivity {
                     matrix.postRotate(90);
                     Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
                     Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
-                    pathLoaded=saveToInternalStorage(rotatedBitmap);
-                    loadImageFromStorage(pathLoaded);
-                    textTargetUri.setText(pathLoaded);
+                     /*pathLoaded=PhotoController.saveToInternalStorage(rotatedBitmap);
+                    Bitmap test1 = PhotoController.loadImageFromStorage(pathLoaded,imageName);
+                    textTargetUri.setText(pathLoaded);*/
+                    String test = PhotoController.imageToString(rotatedBitmap);
+                    imageView.setImageBitmap(PhotoController.stringToImage(test));
                 } catch (FileNotFoundException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -124,68 +131,6 @@ public class TakePhotoActivity extends AppCompatActivity {
                 Toast.makeText(this, "Load Photo Cancelled", Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    private String saveToInternalStorage(Bitmap bitmapImage){
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        // path to /data/user/0/com.example.healthtracker/app_imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",Locale.CANADA).format(new Date());
-        imageName = "test_"+timeStamp+".jpg";
-        File mypath=new File(directory,imageName);
-
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(mypath);
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                assert fos != null;
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return directory.getAbsolutePath();
-    }
-
-    private void loadImageFromStorage(String path) {
-        try {
-            File f=new File(path, imageName);
-            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
-            ImageView img=findViewById(R.id.imageView);
-            img.setImageBitmap(b);
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    private String imageToString(String path, String imageName){
-        String filepath = path+imageName;
-        File imagefile = new File(filepath);
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(imagefile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        Bitmap bm = BitmapFactory.decodeStream(fis);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bm.compress(Bitmap.CompressFormat.JPEG, 100 , baos);
-        byte[] b = baos.toByteArray();
-        return Base64.encodeToString(b, Base64.DEFAULT);
-    }
-
-    private Bitmap stringToImage(String encodedImage){
-        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
     }
 }
 
